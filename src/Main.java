7a34eacbe7e18414fdaec81c0b408dc954b0664d
kirillnice <nice.kirill.sergeevich@gmail.com>
2022-02-03 21:53:56 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int bonusMilesPrice = 20;
+        int bonusMilesAmount = (ticketPrice / bonusMilesPrice);{
+System.out.println("КОЛИЧЕСТВО БОНУСНЫХ МИЛЬ НАЧИСЛЕНО : "+bonusMilesAmount);
+}}}
